356983846cf78a0e4e30c3f347fd5a3bbd185976
Abdou <sall.abdou34@gmail.com>
2024-08-02 14:52:53 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Counter } from './features/counter/Counter.jsx' 
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Counter /> 
+    </>
+  )
+}
+
+export default App
