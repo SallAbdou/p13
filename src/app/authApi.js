c48ffef1e4bd1bf2fd3002917e0a5d6e9b4ce7b5
Abdou <sall.abdou34@gmail.com>
2024-08-17 06:30:40 +0200

    auth/login pages

---
+++ src/app/authApi.js
@@ -0,0 +1,12 @@
+const URL_LOGIN = 'http://localhost:3001/api/v1/user/login'
+
+export const postLogin = data => {
+    return fetch(URL_LOGIN, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Pages/Home.jsx';
 import './index.css'
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header.jsx';
 import Footer from './Components/Footer.jsx';
 import NotFound from './Pages/Notfound.jsx';
+import Login from './Pages/Login.jsx';
+import { store } from './app/store.js';
+import Profile from './Pages/Profile.jsx';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <main>
+  <Provider store={store}>
+    <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/sign-in' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer/>
-    </main>
-  </Router>
+      <Footer />
+    </Router>
+  </Provider>
+
 );
