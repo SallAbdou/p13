597b78121ab8554f4f2ac7bdfbadd741d58a6629
Abdou <sall.abdou34@gmail.com>
2024-08-23 14:06:56 +0200

    test editprofile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,44 +1,36 @@
+
+import { useSelector } from 'react-redux';
+import EditProfile from './EditProfile';
+
 const Profile = () => {
+  const user = useSelector((state) => state.auth.user);
 
-    return (
-        <main className='main'>
-            <section id="user" className="bg-dark">
-                <div className="accounts">
-                    <h2 className="sr-only">Accounts</h2>
-                    <section className="account">
-                        <div className="account-content-wrapper">
-                            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                            <p className="account-amount">$2,082.79</p>
-                            <p className="account-amount-description">Available Balance</p>
-                        </div>
-                        <div className="account-content-wrapper cta">
-                            <button className="transaction-button">View transactions</button>
-                        </div>
-                    </section>
-                    <section className="account">
-                        <div className="account-content-wrapper">
-                            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-                            <p className="account-amount">$10,928.42</p>
-                            <p className="account-amount-description">Available Balance</p>
-                        </div>
-                        <div className="account-content-wrapper cta">
-                            <button className="transaction-button">View transactions</button>
-                        </div>
-                    </section>
-                    <section className="account">
-                        <div className="account-content-wrapper">
-                            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-                            <p className="account-amount">$184.30</p>
-                            <p className="account-amount-description">Current Balance</p>
-                        </div>
-                        <div className="account-content-wrapper cta">
-                            <button className="transaction-button">View transactions</button>
-                        </div>
-                    </section>
-                </div>
-            </section>
-        </main>
-    )
-}
+  return (
+    <main className='main'>
+      <section id="user" className="bg-dark">
+        <div className="accounts">
+          <h2 className="sr-only">Accounts</h2>
+          {/* Affichage des informations du compte */}
+          <section className="account">
+            <div className="account-content-wrapper">
+              <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+              <p className="account-amount">$2,082.79</p>
+              <p className="account-amount-description">Available Balance</p>
+            </div>
+            <div className="account-content-wrapper cta">
+              <button className="transaction-button">View transactions</button>
+            </div>
+          </section>
+          {/* Ajoutez les autres sections de compte ici */}
+        </div>
+      </section>
+      <section id="edit-profile">
+        <h2>Profile Information</h2>
+        <p>Welcome, {user?.firstName} {user?.lastName}!</p>
+        <EditProfile />
+      </section>
+    </main>
+  );
+};
 
-export default Profile
+export default Profile;
